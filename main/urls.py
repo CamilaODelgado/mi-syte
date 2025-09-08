f1d7da9595e82edd3b3905062391564b1199fbd4
CamilaODelgado <o.delgadocamila@gmail.com>
2025-09-08 00:37:43 -0300

    first commit

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+app_name='main' ### necesario para poner en el views.py y redirreccionar
+
+##junto con el de: views.homepage, name='homepage'),
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('registro/', views.register, name='registro')
+]
---
+++ main/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect #aqui agregamos el redirect que es para redirreccionar
+
+
+from django.http import HttpResponse
+# Create your views here.
+
+from .models import Curso
+
+#from del registro
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+
+##para poner mensajes
+from django.contrib import messages
+
+def homepage(request):
+    return render(request, 'index.html', {"cursos":Curso.objects.all()})
+
+
+##Registro Usuario
+
+def register(request):
+    #
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            usuario = form.save() #guarda el usuario
+            
+            nombre_usuario = form.cleaned_data.get('username')#nuevo esto manda mensajes
+            messages.success(request, f'Nueva Cuenta creada para: {nombre_usuario}')
+            
+            login(request, usuario) #logea al usuario
+            return redirect('main:homepage') #redirecciona a la pagina principal
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f'{msg}: {form.error_messages[msg]}') ##msg es una llave para mostrar al usuario en caso de error
+    #
+    form = UserCreationForm()
+    return render(request, 'registro.html', {"form":form}) #recomendable aca utilizar el mismo nombre en la variable
